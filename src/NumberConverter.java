6d4457da75009264fc1472ce4903c0440ce2c819
Charles <cam213@case.edu>
2012-11-12 12:24:11 -0500

    Began convertNumber method, added private methods to break down number conversion
    Signed-off-by: Charles <cam213@case.edu>

--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -15,4 +15,48 @@ public class NumberConverter
 		
 		//Convert and print
 	}
+	
+	public int convertNumber(ArrayList<String> input)
+	{
+		if(input.get(0) == "naught" || input.get(0) == "zero")
+		{
+			return 0;
+		}
+		
+		int number = 0;
+		boolean negative = false;
+		while(!input.isEmpty())
+		{
+			if(input.contains("negative") || input.contains("minus"))
+			{
+				negative = true;
+				input.remove("negative");
+				input.remove("positive");
+			}
+			else if(input.contains("million"))
+			{
+				number += million(input.subList(0, input.indexOf("million")));
+			}
+		}
+	}
+	
+	private int million(ArrayList<String> subList)
+	{
+		
+	}
+	
+	private int hundred(ArrayList<String> subList)
+	{
+		
+	}
+	
+	private int ten(ArrayList<String> subList)
+	{
+		
+	}
+	
+	private int one(String num)
+	{
+		
+	}
 }
