347f16e3c0174a69949b9fea1b434011015bcc46
Charles <cam213@case.edu>
2012-11-12 11:58:09 -0500

    Created NumberConverter class, started main method, added Number class
    Signed-off-by: Charles <cam213@case.edu>

---
+++ src/NumberConverter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class NumberConverter
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		ArrayList<String> lines = new ArrayList<String>();
+		
+		while(in.hasNext())
+		{
+			lines.add(in.nextLine());
+		}
+		
+		//Convert and print
+	}
+}
